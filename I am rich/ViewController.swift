e609bc338c825199faa3b1ecb5752b65c0919607
tanishq14developer <tanishq.happy@gmail.com>
2022-04-09 12:28:34 +0530

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by TANISHQ CHAWDA on 09/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
